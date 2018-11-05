c3b7c48a1e912bb3bef101e6318379b559b785ab
MaxKonopelko <workmax1993@gmail.com>
2018-11-05 19:45:35 +0300

    fix base buttons

--- src/app/main/mainComponent.ts
+++ src/app/main/mainComponent.ts
@@ -13,23 +13,25 @@ export class MainComponent implements IComponent
 
   public onInit(): void
   {
-    document.getElementById('fl1').addEventListener('click', this.handleSubmit);
-    document.getElementById('fl2').addEventListener('click', this.handleSubmit);
-    document.getElementById('fl3').addEventListener('click', this.handleSubmit);
+    this.handleSubmit(null, this._imagesComponent);
+
+    document.getElementById('fl1').addEventListener('click', (e) => this.handleSubmit(e, this._imagesComponent));
+    document.getElementById('fl2').addEventListener('click', (e) => this.handleSubmit(e, this._musicsComponent));
+    document.getElementById('fl3').addEventListener('click', (e) => this.handleSubmit(e, this._filmsComponent));
   }
 
-  private handleSubmit = () =>
+  private handleSubmit = (e: Event, component: IComponent) =>
   {
-    document.getElementById('content').innerHTML = this._imagesComponent.template();
+    document.getElementById('content').innerHTML = component.template();
   };
 
   public template(): string
   {
     return `
             <div class="menu">
-                 <div class="b1" id="fl1"><i class="fa fa-youtube-play" style="font-size:36px"></i>Фильмы</div>
+                 <div class="b1" id="fl1"><i class="fa fa-youtube-play" style="font-size:36px"></i>Фото</div>
                  <div class="b1" id="fl2"><i class="fa fa-music" style="font-size:36px"></i>Музыка</div>
-                 <div class="b1" id="fl3"><i class="fa fa-photo" style="font-size:36px;"></i>Фото</div>
+                 <div class="b1" id="fl3"><i class="fa fa-photo" style="font-size:36px;"></i>Фильмы</div>
             </div>
             <div class="content" id="content"></div>`;
   }
