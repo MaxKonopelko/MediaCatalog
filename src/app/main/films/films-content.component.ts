e695795ae99e25e3761fe006115cf02a6e76e5a7
MaxKonopelko <workmax1993@gmail.com>
2018-12-02 02:09:32 +0300

    fix test

--- src/app/main/films/films-content.component.ts
+++ src/app/main/films/films-content.component.ts
@@ -1,4 +1,15 @@
 class FilmsContentComponent
 {
-
+    public template(): string
+    {
+      return `
+                <div class= "photo-content">  
+                       <div class="photo">
+                          <div class="photo-size">                                       
+                              <img class="image" id="image" src="${urlDefaultImage}">
+                          </div>
+                       </div>
+                </div>
+      `;
+    }          
 }
